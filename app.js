242bce184edca16604d3365ec0b0b5bc3eafc9ca
Arun Prakash Pandey <131158288+D32SSV@users.noreply.github.com>
2023-07-29 19:35:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-    function fetchCryptocurrencyData() {
-        const timestamp = new Date().getTime(); 
-        const apiUrl = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en&_=${timestamp}`;
-        return fetch(apiUrl)
-          .then(response => response.json())
-          .catch(error => console.error('Error fetching data:', error));
-      }
-  
+
+function fetchCryptocurrencyData() {
+  const apiUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en';
+  return fetch(apiUrl)
+    .then(response => response.json())
+    .catch(error => console.error('Error fetching data:', error));
+}
+
   
       function createGridViewCard(crypto) {
         const card = document.createElement('div');
